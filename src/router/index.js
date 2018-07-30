16aa2f5658e64b38db8de0cb6e84cb898e4e7ec1
eyea <deyesea@gmail.com>
2018-07-30 19:20:12 +0800

    add echarts

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Index from '@/components/Index';
+import VueEcharts from '@/components/VueEcharts';
 
 Vue.use(Router);
 
+const routes = [{
+  path: '/',
+  name: 'Index',
+  component: Index,
+}, {
+  path: '/vue-echarts',
+  name: 'VueEcharts',
+  component: VueEcharts,
+}];
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-  ],
+  mode: 'history', // 不配置这个路由带#号 location.href时候
+  routes,
 });
+// Hash: 使用URL的hash值来作为路由。支持所有浏览器。
+// History: 以来HTML5 History API 和服务器配置。参考官网中HTML5 History模式
+// Abstract： 支持所有javascript运行模式。如果发现没有浏览器的API，路由会自动强制进入这个模式。
